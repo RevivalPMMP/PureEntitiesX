f1fcd0ad50087c9bb807eaccd880e390837df57e
승원 <milk5590@naver.com>
2016-02-27 12:13:42 +0900

    Spawner Update (V1)

--- src/milk/pureentities/tile/Spawner.php
+++ src/milk/pureentities/tile/Spawner.php
@@ -2,15 +2,68 @@
 
 namespace milk\pureentities\tile;
 
+use pocketmine\level\format\FullChunk;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ShortTag;
 use pocketmine\nbt\tag\StringTag;
 use pocketmine\tile\Spawnable;
 
 class Spawner extends Spawnable{
-    //TODO: This isn't implemeted yet
 
-    public $entityId = -1;
+    protected $entityId = -1;
+    protected $spawnRange;
+    protected $maxNearbyEntities;
+    protected $requiredPlayerRange;
+
+    protected $delay = 0;
+
+    protected $minSpawnDelay;
+    protected $maxSpawnDelay;
+
+    public function __construct(FullChunk $chunk, CompoundTag $nbt){
+        parent::__construct($chunk, $nbt);
+
+        if(isset($this->namedtag->EntityId)){
+            $this->entityId = $this->namedtag["EntityId"];
+        }
+
+        if(!isset($this->namedtag->SpawnRange)){
+            $this->namedtag->SpawnRange = new ShortTag("SpawnRange", 25);
+        }
+
+        if(!isset($this->namedtag->MinSpawnDelay)){
+            $this->namedtag->MinSpawnDelay = new ShortTag("MinSpawnDelay", 200);
+        }
+
+        if(!isset($this->namedtag->MaxSpawnDelay)){
+            $this->namedtag->MaxSpawnDelay = new ShortTag("MaxSpawnDelay", 8000);
+        }
+
+        if(!isset($this->namedtag->MaxNearbyEntities)){
+            $this->namedtag->MaxNearbyEntities = new ShortTag("MaxNearbyEntities", 25);
+        }
+
+        if(!isset($this->namedtag->RequiredPlayerRange)){
+            $this->namedtag->RequiredPlayerRange = new ShortTag("RequiredPlayerRange", 20);
+        }
+
+        $this->spawnRange = $this->namedtag["SpawnRange"];
+        $this->minSpawnDelay = $this->namedtag["MinSpawnDelay"];
+        $this->maxSpawnDelay = $this->namedtag["MaxSpawnDelay"];
+        $this->maxNearbyEntities = $this->namedtag["MaxNearbyEntities"];
+        $this->requiredPlayerRange = $this->namedtag["RequiredPlayerRange"];
+
+        $this->scheduleUpdate();
+    }
+
+    public function onUpdate(){
+        if($this->closed){
+            return false;
+        }
+
+        return true;
+    }
 
     public function getSpawnCompound(){
         return new CompoundTag("", [
