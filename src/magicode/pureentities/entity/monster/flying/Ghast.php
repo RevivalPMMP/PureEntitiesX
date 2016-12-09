8bad21f4b9e4a63edfedfd66aabbd46bf11b6b48
Blabla <st.ten.veldhuis@gmail.com>
2016-12-09 16:56:05 +0100

    Fix unexpected end of file crash.

--- src/magicode/pureentities/entity/monster/flying/Ghast.php
+++ src/magicode/pureentities/entity/monster/flying/Ghast.php
@@ -80,6 +80,7 @@ class Ghast extends FlyingMonster implements ProjectileSource{
     public function getDrops(){
         if($this->lastDamageCause instanceof EntityDamageByEntityEvent){
             return [Item::get(Item::GUNPOWDER, 0, mt_rand(0, 2))];
+        }
         return [];
     }
 
