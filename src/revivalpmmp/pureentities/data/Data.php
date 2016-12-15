f6a2b124fcdd5d7adc3219f0464eb1c0495c1d8b
Joshua <jross3740@gmail.com>
2016-12-15 16:45:30 -0500

    I'll add more but this is what I meant :)

--- src/revivalpmmp/pureentities/data/Data.php
+++ src/revivalpmmp/pureentities/data/Data.php
@@ -1,1 +1,23 @@
+<?php
+namespace revivalpmmp\pureentities\data;
 
+interface Data{
+  
+  const CHICKEN = 10;
+  const COW = 11;
+  const PIG = 12;
+  const SHEEP = 13;
+  const VILLAGER = 15;
+  const MOOSHROOM = 16;
+  const RABBIT = 18;
+  const OCELOT = 22;
+  const HORSE = 23;
+  const DONKEY = 24;
+  const ZOMBIE = 32;
+  const CREEPER = 33;
+  const SKELETON = 34;
+  const SPIDER = 35;
+  const ZOMBIE_PIGMAN = 36;
+  const ENDERMAN = 38;
+  const ZOMBIE_VILLAGER= 44;
+}
