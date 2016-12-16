01a839a2b7b7dbe19e45ddcfbf2e392e12a425b1
Blabla <st.ten.veldhuis@gmail.com>
2016-12-16 09:57:50 +0100

    Update Sheep.php

--- src/revivalpmmp/pureentities/entity/animal/walking/Sheep.php
+++ src/revivalpmmp/pureentities/entity/animal/walking/Sheep.php
@@ -29,7 +29,7 @@ class Sheep extends WalkingAnimal implements Colorable{
         if($creature instanceof Player){
             if($creature->getInventory()->getItemInHand()->getId() === Item::SEEDS) {
                 return $creature->spawned && $creature->isAlive() && !$creature->closed && $distance <= 49;
-            } elseif($creature->getInventory()->getItemInHand()->getId() === Item::SHEARS && $this instanceof self::class) {
+            } elseif($creature->getInventory()->getItemInHand()->getId() === Item::SHEARS && $this instanceof $this) {
                 $creature->setDataProperty(self::DATA_INTERACTIVE_TAG, self::DATA_TYPE_STRING, "Shear");
                 return $distance <= 4;
             }
