2415c1822e1ee1841b95f32a1e80d5f13357519d
Sandertv <st.ten.veldhuis@gmail.com>
2016-12-16 16:48:03 +0100

    Fix button showing up for other entities.

--- src/revivalpmmp/pureentities/entity/animal/walking/Sheep.php
+++ src/revivalpmmp/pureentities/entity/animal/walking/Sheep.php
@@ -33,7 +33,7 @@ class Sheep extends WalkingAnimal implements Colorable{
             } elseif($creature->getInventory()->getItemInHand()->getId() === Item::SHEARS && $this instanceof Sheep && $this->getDataFlag(self::DATA_FLAGS, self::DATA_FLAG_SHEARED) === false) {
                 $creature->setDataProperty(self::DATA_INTERACTIVE_TAG, self::DATA_TYPE_STRING, "Shear");
             } else {
-                $creature->setDataProperty(self::DATA_INTERACTIVE_TAG, self::DATA_TYPE_STRING, "");
+                $creature->setDataProperty(self::DATA_INTERACTIVE_TAG, self::DATA_TYPE_STRING, 0);
             }
         }
         return false;
