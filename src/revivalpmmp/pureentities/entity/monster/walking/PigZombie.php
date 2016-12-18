4497460feb3a8c791f85344223b5f7f159eb1e1e
tjespe <tordjohanespe@tord.local>
2016-12-18 12:36:14 +0100

    Pig zombie

--- src/revivalpmmp/pureentities/entity/monster/walking/PigZombie.php
+++ src/revivalpmmp/pureentities/entity/monster/walking/PigZombie.php
@@ -89,17 +89,8 @@ class PigZombie extends WalkingMonster{
     public function getDrops(){
         $drops = [];
         if($this->lastDamageCause instanceof EntityDamageByEntityEvent){
-            switch(mt_rand(0, 2)){
-                case 0:
-                    $drops[] = Item::get(Item::GOLD_INGOT, 0, 1);
-                    break;
-                case 1:
-                    $drops[] = Item::get(Item::GOLD_SWORD, 0, 1);
-                    break;
-                case 2:
-                    $drops[] = Item::get(Item::ROTTEN_FLESH, 0, 1);
-                    break;
-            }
+            array_push($drops, Item::get(Item::ROTTEN_FLESH, 0, mt_rand(0, 1)));
+            array_push($drops, Item::get(Item::GOLD_INGOT, 0, mt_rand(0, 1)));
         }
         return $drops;
     }
