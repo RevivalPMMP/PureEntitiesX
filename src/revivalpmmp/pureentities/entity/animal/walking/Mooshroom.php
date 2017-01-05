74a7358cf1240a7e4d10bd7f273e7acbe49605b4
samalero <ivan.barabash96@mail.ru>
2017-01-05 10:10:21 +0200

    Update Mooshroom.php

--- src/revivalpmmp/pureentities/entity/animal/walking/Mooshroom.php
+++ src/revivalpmmp/pureentities/entity/animal/walking/Mooshroom.php
@@ -22,7 +22,7 @@ class Mooshroom extends WalkingAnimal{
     public function initEntity(){
         parent::initEntity();
 
-        $this->setMaxHealth(10);
+        $this->setMaxHealth(20);
     }
 
     public function targetOption(Creature $creature, float $distance) : bool{
