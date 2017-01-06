14147e658a7c38daab70f6e3d4b67e8a438c2b2d
caspervanneck <nethercube.host@gmail.com>
2017-01-06 15:41:36 +0100

    Add files via upload

---
+++ src/revivalpmmp/pureentities/entity/monster/walking/ExplosionPrimeEvent.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace revivalpmmp\pureentities\event;
+
+use pocketmine\entity\Entity;
+use pocketmine\event\Cancellable;
+use pocketmine\event\entity\EntityEvent;
+
+
+/**
+ * Called when a entity decides to explode
+ */
+class ExplosionPrimeEvent extends EntityEvent implements Cancellable{
+	public static $handlerList = null;
+
+	protected $force;
+	private $blockBreaking;
+
+	/**
+	 * @param Entity $entity
+	 * @param float  $force
+	 */
+	public function __construct(Entity $entity, $force){
+		$this->entity = $entity;
+		$this->force = $force;
+		$this->blockBreaking = true;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getForce(){
+		return $this->force;
+	}
+
+	public function setForce($force){
+		$this->force = (float) $force;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isBlockBreaking(){
+		return $this->blockBreaking;
+	}
+
+	/**
+	 * @param bool $affectsBlocks
+	 */
+	public function setBlockBreaking($affectsBlocks){
+		$this->blockBreaking = (bool) $affectsBlocks;
+	}
+
+}
