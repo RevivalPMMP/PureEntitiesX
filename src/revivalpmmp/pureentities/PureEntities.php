490960f4b7ee52b1147cfa9109efa6e88ddce569
caspervanneck <nethercube.host@gmail.com>
2017-01-06 09:18:56 +0100

    Update PureEntities.php

--- src/revivalpmmp/pureentities/PureEntities.php
+++ src/revivalpmmp/pureentities/PureEntities.php
@@ -135,8 +135,7 @@ class PureEntities extends PluginBase {
             ZombieVillager::class,
             FireBall::class
         ];
-	    
-      $this->registerTiles();
+	   
 	    
         foreach($classes as $name){
             Entity::registerEntity($name);
@@ -154,7 +153,6 @@ class PureEntities extends PluginBase {
             }
         }
 
- 	private function registerTiles(){
  		Tile::registerTile(Spawner::class);
 				
         $this->getServer()->getLogger()->info(TextFormat::GOLD . "[PureEntitiesX] The Original Code for this Plugin was Written by milk0417. It is now being maintained by RevivalPMMP for PMMP 'Unleashed'.");
