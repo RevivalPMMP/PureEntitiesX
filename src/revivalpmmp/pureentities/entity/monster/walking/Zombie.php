5292e754fe1734c7e22f133ee84143623d11a9b3
xinghao2003 <wongxh45@gmail.com>
2017-01-07 21:20:19 +0800

    Update Zombie.php

--- src/revivalpmmp/pureentities/entity/monster/walking/Zombie.php
+++ src/revivalpmmp/pureentities/entity/monster/walking/Zombie.php
@@ -64,7 +64,7 @@ class Zombie extends WalkingMonster implements Ageable{
         }
     }
 
-    public function entityBaseTick($tickDiff = 1){
+    public function entityBaseTick($tickDiff = 1, $EnchantL = 0){
         Timings::$timerEntityBaseTick->startTiming();
 
         $hasUpdate = parent::entityBaseTick($tickDiff);
