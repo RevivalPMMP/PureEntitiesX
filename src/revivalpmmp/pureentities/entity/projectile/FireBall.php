07136d5f0bd563ac4186aaa5064ced7e78142b45
caspervanneck <nethercube.host@gmail.com>
2017-01-08 10:01:15 +0100

    Update FireBall.php

--- src/revivalpmmp/pureentities/entity/projectile/FireBall.php
+++ src/revivalpmmp/pureentities/entity/projectile/FireBall.php
@@ -29,7 +29,8 @@ use pocketmine\Player;
 use pocketmine\entity\Projectile;
 use pocketmine\entity\Entity;
 use pocketmine\level\Explosion;
-use pocketmine\event\entity\ExplosionPrimeEvent;
+use pocketmine\level\format\Chunk;
+use revivalpmmp\pureentities\entity\monster\walking\ExplosionPrimeEvent;
 
 class FireBall extends Projectile{
     const NETWORK_ID = 85;
