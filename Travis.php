2217f3787017086790a0684ecab604d44b184cd0
Magicode <herbmcsherb@gmail.com>
2017-01-09 20:06:07 -0500

    Create Travis.php
    This is the same Travis.php that Robske110 on his BanWarn Plugin. I give him full credit.

---
+++ Travis.php
@@ -0,0 +1,21 @@
+<?php
+$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
+	0 => ["pipe", "r"],
+	1 => ["pipe", "w"],
+	2 => ["pipe", "w"]
+], $pipes);
+fwrite($pipes[0], "version\nmakeplugin BanWarn\nstop\n\n");
+while(!feof($pipes[1])){
+	echo fgets($pipes[1]);
+}
+fclose($pipes[0]);
+fclose($pipes[1]);
+fclose($pipes[2]);
+echo "\n\nReturn value: ". proc_close($server) ."\n";
+if(count(glob("plugins/DevTools/PureEntitiesX*.phar")) === 0){
+	echo "Failed to create PureEntitiesX phar!\n";
+	exit(1);
+}else{
+	echo "PureEntitiesX phar created!\n";
+	exit(0);
+}
