7adfa2441671859480046650228263e4005b28d3
jasonwynn10 <jasonwynn10@gmail.com>
2017-01-22 11:36:06 -0500

    Added License Headers and removed unused dependencies

--- src/revivalpmmp/pureentities/entity/monster/flying/Blaze.php
+++ src/revivalpmmp/pureentities/entity/monster/flying/Blaze.php
@@ -1,5 +1,21 @@
 <?php
 
+/*  PureEntitiesX: Mob AI Plugin for PMMP
+    Copyright (C) 2016 RevivalPMMP
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 namespace revivalpmmp\pureentities\entity\monster\flying;
 
 use revivalpmmp\pureentities\entity\animal\Animal;
@@ -12,7 +28,6 @@ use pocketmine\block\Slab;
 use pocketmine\block\Stair;
 use pocketmine\entity\Creature;
 use pocketmine\entity\Entity;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\ProjectileLaunchEvent;
 use pocketmine\entity\ProjectileSource;
 use pocketmine\item\Item;
--- src/revivalpmmp/pureentities/entity/monster/walking/WitherSkeleton.php
+++ src/revivalpmmp/pureentities/entity/monster/walking/WitherSkeleton.php
@@ -1,15 +1,29 @@
 <?php
 
+/*  PureEntitiesX: Mob AI Plugin for PMMP
+    Copyright (C) 2016 RevivalPMMP
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 namespace revivalpmmp\pureentities\entity\monster\walking;
 
 use revivalpmmp\pureentities\entity\monster\WalkingMonster;
 use pocketmine\entity\Entity;
 use pocketmine\item\StoneSword;
-use pocketmine\nbt\tag\IntTag;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\item\Item;
-use pocketmine\entity\Creature;
 use pocketmine\network\protocol\MobEquipmentPacket;
 use pocketmine\Player;
 use revivalpmmp\pureentities\data\Data;
@@ -66,7 +80,7 @@ class WitherSkeleton extends WalkingMonster{
         array_push($drops, Item::get(Item::BONE, 0, mt_rand(0, 2)));
         switch (mt_rand(0, 8)) {
           case 1:
-            array_push($drops, Item::get(Item::WITHER_SKELETON_SKULL, 0, mt_rand(0, 2)));
+            array_push($drops, Item::get(Item::SKULL, 1, mt_rand(0, 2)));
             break;
         }
         return $drops;
