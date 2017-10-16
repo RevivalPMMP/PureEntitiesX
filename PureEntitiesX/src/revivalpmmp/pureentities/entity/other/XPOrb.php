b200dbe53d36acf373f157ddadb993e3896588fa
95CivicSi <aaron_salyer@yahoo.com>
2017-10-16 07:44:50 -0400

    Another Xp Update
    This moves the Xp calculations to their own class under the utilities namespace.  Thanks @jasonwynn10 for the suggestion.

--- PureEntitiesX/src/revivalpmmp/pureentities/entity/other/XPOrb.php
+++ PureEntitiesX/src/revivalpmmp/pureentities/entity/other/XPOrb.php
@@ -18,14 +18,12 @@
 namespace revivalpmmp\pureentities\entity\other;
 
 use pocketmine\entity\Entity;
-use pocketmine\math\Vector3;
-use pocketmine\nbt\tag\FloatTag;
-use pocketmine\nbt\tag\IntTag;
 use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\Player;
 use revivalpmmp\pureentities\PluginConfiguration;
-use revivalpmmp\pureentities\PureEntities;
 use revivalpmmp\pureentities\sound\ExpPickupSound;
+use revivalpmmp\pureentities\utils\XpCalculator;
+
 class XPOrb extends Entity {
 	const NETWORK_ID = 69;
 
@@ -113,7 +111,7 @@ class XPOrb extends Entity {
                     if ($this->getLevel() !== null) {
                         $this->level->addSound(new ExpPickupSound($target, mt_rand(0, 1000)));
                     }
-                    $this->updatePlayerXp($target, $xpToGain);
+                    XpCalculator::updatePlayerXp($target, $xpToGain);
                 }
                 $this->timings->stopTiming();
 
@@ -154,80 +152,4 @@ class XPOrb extends Entity {
 
 		parent::spawnTo($player);
 	}
-
-
-    /**
-     * This function handles updating player XP levels and progress when collecting an Xp Orb.
-     *
-     * @param Player $player
-     * @param int $xpGained
-     */
-	private function updatePlayerXp(Player $player, int $xpGained) {
-        $playerXpLevel = $player->getXpLevel();
-        $playerXpProgress = $player->getXpProgress();
-
-        $playerXpLevelGap = $this->getXpLevelGap($player->getXpLevel());
-        $playerCurrentXp = ($playerXpProgress * $playerXpLevelGap) + $xpGained;
-        $updatePlayer = true;
-
-        while ($updatePlayer) {
-
-            // Update internal progress counter with new Xp information
-            $playerXpProgress = $playerCurrentXp / $playerXpLevelGap;
-
-            if ($playerXpProgress >= 1) {
-                $playerXpLevel++;
-                $playerCurrentXp = $playerCurrentXp - $playerXpLevelGap;
-                $playerXpLevelGap = $this->getXpLevelGap($player->getXpLevel());
-            } else {
-                $player->setXpLevel($playerXpLevel);
-                $player->namedtag->XpLevel = new IntTag("XpLevel", $playerXpLevel);
-                $player->setXpProgress($playerXpProgress);
-                $player->namedtag->XpP = new FloatTag("XpP", $playerXpProgress);
-                $updatePlayer = false;
-            }
-        }
-    }
-
-    /**
-     * This function calculates and returns the total amount of Xp required to reach the level passed in.
-     * Level values passed into this function should be positive integers or zero.
-     *
-     * @param int $level
-     * @return int
-     */
-    private function calculateLevelXp(int $level = 1) : int {
-        // This will return the total amount of Xp required to reach the level passed into the function.
-
-        if ($level == 0) {
-            return 0;
-        } elseif ($level >= 1 and $level <= 16) {
-            return (($level ** 2) + (6 * $level));
-        } elseif ($level >= 17 and $level <= 31) {
-            return (2.5 * ($level ** 2) - (40.5 * $level) + 360);
-        } elseif ($level >= 32) {
-            return (4.5 * ($level ** 2) - (162.5 * $level) + 2220);
-        } else {
-            PureEntities::logOutput("$this: CalculateLevelXp received invalid level $level", PureEntities::CRITICAL);
-            return -1;
-        }
-    }
-
-    /**
-     * This is the amount of Xp required to reach the next level from the player's
-     * current level without considering the player's total Xp.
-     *
-     * eg.  If the player is currently on Xp Level 7, then they had to have a minimum
-     * of 91 Xp to reach that level.  To reach Xp Level 8, they need a total Xp of 112.
-     * The difference from what is needed to reach Xp Level 8 and what is needed
-     * to reach Xp Level 7 is 112 - 91 = 21.
-     * So this would return 21.
-     *
-     * @param int $currentLevel
-     * @return int
-     *
-     */
-    private function getXpLevelGap(int $currentLevel) : int {
-        return ($this->calculateLevelXp($currentLevel + 1) - $this->calculateLevelXp($currentLevel));
-    }
 }
