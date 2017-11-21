3a85ea9c41aad4a2b7485a55dbfda542f2293a08
Aaron Salyer <aaron_salyer@yahoo.com>
2017-11-21 05:45:16 -0500

    Completely Remove Individual Constants for Network IDs

--- src/revivalpmmp/pureentities/data/Data.php
+++ src/revivalpmmp/pureentities/data/Data.php
@@ -21,51 +21,6 @@ namespace revivalpmmp\pureentities\data;
 
 interface Data{
 
-	// Network IDs
-	/* Individual Network ID Constants will soon be phased out
-	 * and replaced with the NETWORK_IDS array.
-	 */
-
-	/*
-		const CHICKEN = 10;
-		const COW = 11;
-		const PIG = 12;
-		const SHEEP = 13;
-		const VILLAGER = 15;
-		const MOOSHROOM = 16;
-		const RABBIT = 18;
-		const OCELOT = 22;
-		const HORSE = 23;
-		const DONKEY = 24;
-		const ZOMBIE = 32;
-		const CREEPER = 33;
-		const SKELETON = 34;
-		const SPIDER = 35;
-		const ZOMBIE_PIGMAN = 36;
-		const ENDERMAN = 38;
-		const ZOMBIE_VILLAGER = 44;
-		const WITHER_SKELETON = 48;
-		const WOLF = 14;
-		const SQUID = 17;
-		const IRON_GOLEM = 20;
-		const SNOW_GOLEM = 21;
-		const MULE = 25;
-		const STRAY = 46;
-		const BAT = 19;
-		const PIG_ZOMBIE = 36;
-		const SILVERFISH = 39;
-		const CAVE_SPIDER = 40;
-		const GHAST = 41;
-		const BLAZE = 43;
-		const MAGMA_CUBE = 42;
-		const HUSK = 47;
-		const GUARDIAN = 49;
-		const ELDER_GUARDIAN = 50;
-		const SLIME = 37;
-		const FIRE_BALL = 85;
-		const PARROT = 30;
-	*/
-
 	// Entity Network IDs
 	const NETWORK_IDS = array(
 		"bat" => 19,
@@ -133,7 +88,7 @@ interface Data{
 		self::NETWORK_IDS["parrot"] => 0.5,
 		self::NETWORK_IDS["pig"] => 1.5,
 		self::NETWORK_IDS["pig_zombie"] => 1.125,
-		self::NETWORK_IDS["rabbit"] => 0.5,
+		self::NETWORK_IDS["rabbit"] => 0.4,
 		self::NETWORK_IDS["sheep"] => 1.2,
 		self::NETWORK_IDS["silverfish"] => 1.094,
 		self::NETWORK_IDS["skeleton"] => 0.875,
