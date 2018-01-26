2c24af568c7c7081bc5242ceaf36fe5ddae7a2c8
Aaron Salyer <aaron_salyer@yahoo.com>
2018-01-26 09:36:47 -0500

    Reduce function calls to increase performance.

--- src/revivalpmmp/pureentities/utils/MobDamageCalculator.php
+++ src/revivalpmmp/pureentities/utils/MobDamageCalculator.php
@@ -81,10 +81,7 @@ class MobDamageCalculator{
 		if($player instanceof Player and $player->getArmorInventory() !== null){
 			$playerArmor = $player->getArmorInventory();
 			$armorItems = [];
-			array_push($armorItems, $playerArmor->getHelmet());
-			array_push($armorItems, $playerArmor->getChestplate());
-			array_push($armorItems, $playerArmor->getLeggings());
-			array_push($armorItems, $playerArmor->getBoots());
+			array_push($armorItems, $playerArmor->getHelmet(), $playerArmor->getChestplate(), $playerArmor->getLeggings(), $playerArmor->getBoots());
 
 			if($armorItems !== null and sizeof($armorItems) > 0){
 				// complete damage reduction in percent
