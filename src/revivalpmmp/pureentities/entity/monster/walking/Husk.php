874d3aa0ef2c804b8f0446b00d8961686c97b174
95CivicSi <aaron_salyer@yahoo.com>
2018-06-17 12:02:00 -0400

    Husk: Refactor setHealth to include void return type.

--- src/revivalpmmp/pureentities/entity/monster/walking/Husk.php
+++ src/revivalpmmp/pureentities/entity/monster/walking/Husk.php
@@ -54,7 +54,7 @@ class Husk extends WalkingMonster implements Ageable{
 		return $this->getDataFlag(self::DATA_FLAG_BABY, 0);
 	}
 
-	public function setHealth(float $amount){
+	public function setHealth(float $amount) : void{
 		parent::setHealth($amount);
 
 		if($this->isAlive()){
